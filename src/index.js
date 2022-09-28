df152e9315ac41888b3b8fb5521b45ddbba52018
yaruo-react-redux <kazuya@goemon-the-web.com>
2022-09-28 16:49:39 -0700

    webpack-config generate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import _ from 'lodash';
+import './style.css';
+import './style.scss';
+
+import Yaruo from './assets/images/yaruo.png';
 
 function component() {
   const element = document.createElement('div');
@@ -9,3 +13,8 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const image = new Image();
+image.src = Yaruo;
+
+document.body.appendChild(image);
